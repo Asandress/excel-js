2a5cb54bd2a268d38fef120bdddb6167415c0ccf
Andrey Sam <samjob@bk.ru>
2021-01-25 17:55:26 +0300

    finish redux logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,17 +5,13 @@ export class ExcelComponent extends DomListener{
         super($root, options.listeners)
         this.name = options.name || ''
         this.emitter = options.emitter
+        this.subscribe = options.subscribe || []
+        this.store = options.store
         this.unsubscribers = []
-
-        console.log(options.emitter)
-
-
         this.prepare()
     }
 
-    prepare() {
-
-    }
+    prepare() {}
 
     //вщзвращает шаблон компонента
     toHTML() {
@@ -28,7 +24,16 @@ export class ExcelComponent extends DomListener{
     $on(event, fn) {
         const unsub = this.emitter.subscribe(event, fn)
         this.unsubscribers.push(unsub)
+    }
 
+    $dispatch(action) {
+        this.store.dispatch(action)
+    }
+    storeChanged() {
+
+    }
+    isWatching(key) {
+        return this.subscribe.includes(key)
     }
 
     init() {
